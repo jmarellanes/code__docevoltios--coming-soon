6ed95d2c5d0cbb5a85a9386c2af2ca3b693e77f9
jmarellanes <josemanuel.arellanes@gmail.com>
2021-03-26 13:13:58 -0700

    First commit - Project Config

---
+++ gulpfile.babel.js
@@ -0,0 +1,103 @@
+import { src, dest, watch, series, parallel } from "gulp";
+import yargs from "yargs";
+import sass from "gulp-sass";
+import cleanCss from "gulp-clean-css";
+import gulpif from "gulp-if";
+import postcss from "gulp-postcss";
+import sourcemaps from "gulp-sourcemaps";
+import autoprefixer from "autoprefixer";
+import imagemin from "gulp-imagemin";
+import del from "del";
+import webpack from "webpack-stream";
+import browserSync from "browser-sync";
+
+const PRODUCTION = yargs.argv.prod;
+const autoReload = browserSync.create();
+
+export const serve = (done) => {
+  autoReload.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  done();
+};
+
+export const reload = (done) => {
+  autoReload.reload();
+  done();
+};
+
+export const clean = () => del(["dist"]);
+
+export const styles = () => {
+  return src("src/scss/bundle.scss")
+    .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulpif(PRODUCTION, postcss([autoprefixer])))
+    .pipe(gulpif(PRODUCTION, cleanCss({ compatibility: "ie8" })))
+    .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+    .pipe(dest("dist/css"))
+    .pipe(autoReload.stream());
+};
+
+export const images = () => {
+  return src("src/images/**/*.{jpg,jpeg,png,svg,gif}")
+    .pipe(gulpif(PRODUCTION, imagemin()))
+    .pipe(dest("dist/images"));
+};
+
+export const copy = () => {
+  return src([
+    "src/**/*",
+    "!src/{images,js,scss}",
+    "!src/{images,js,scss}/**/*",
+  ]).pipe(dest("dist"));
+};
+
+export const scripts = () => {
+  return src("src/js/bundle.js")
+    .pipe(
+      webpack({
+        module: {
+          rules: [
+            {
+              test: /\.js$/,
+              use: {
+                loader: "babel-loader",
+                options: {
+                  presets: [],
+                },
+              },
+            },
+          ],
+        },
+        mode: PRODUCTION ? "production" : "development",
+        devtool: !PRODUCTION ? "inline-source-map" : false,
+        output: {
+          filename: "bundle.js",
+        },
+      })
+    )
+    .pipe(dest("dist/js"));
+};
+
+export const watchForChanges = () => {
+  watch("src/scss/**/*.scss", series(styles));
+  watch("src/images/**/*.{jpg,jpeg,png,svg,gif}", series(images, reload));
+  watch(
+    ["src/**/*", "!src/{images,js,scss}", "!src/{images,js,scss}/**/*"],
+    series(copy, reload)
+  );
+  watch("src/js/**/*.js", series(scripts, reload));
+  watch("**/*.html", reload);
+};
+
+export const dev = series(
+  clean,
+  parallel(styles, images, copy, scripts),
+  serve,
+  watchForChanges
+);
+export const build = series(clean, parallel(styles, images, copy, scripts));
+export default dev;

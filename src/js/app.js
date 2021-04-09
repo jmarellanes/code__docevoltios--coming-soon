79329a0cb8ce440c5ad7044eadd9cdf16c4d54b9
jmarellanes <josemanuel.arellanes@gmail.com>
2021-04-09 04:44:25 -0600

    1. Validation with the -Constraint Validation API- with Javascript 2. Style for error messages

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
-import './components/slider';
-console.log('testing');
+import { validateForm } from './components/contact-form';
 
 // Wait until DOM is ready
 document.addEventListener('DOMContentLoaded', function (event) {
   // Wait until images, links, fonts, stylesheets, and js is loaded
+  validateForm();
   window.onload = function () {
     // Waits until next available screen repaint to run code
     window.requestAnimationFrame(function () {});

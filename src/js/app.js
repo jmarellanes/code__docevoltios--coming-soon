bee7164c8d54b41e0319d08076f18cb9395d0f28
jmarellanes <josemanuel.arellanes@gmail.com>
2021-05-17 16:08:45 -0600

    Add > HTML markup for loader

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,17 @@
 import { openForm } from './components/open-form';
-// import { closeFormButton } from './components/close-form-button';
 import { validateForm } from './components/form-validation';
-import { removeFocusAfterClick } from './partials/utils';
 
-// Wait until DOM is ready
-document.addEventListener('DOMContentLoaded', function (event) {
-  // Wait until images, links, fonts, stylesheets, and js is loaded
-  // removeFocusAfterClick();
-  openForm();
-  // closeFormButton();
-  validateForm();
+const select = (e) => document.querySelector(e);
+const selectAll = (e) => document.querySelectorAll(e);
 
-  window.onload = function () {
-    // Waits until next available screen repaint to run code
-    window.requestAnimationFrame(function () {});
-  };
-});
+// // Wait until DOM is ready
+// document.addEventListener('DOMContentLoaded', function (event) {
+//   // Wait until images, links, fonts, stylesheets, and js is loaded
+//   openForm();
+//   validateForm();
+//
+//   window.onload = function () {
+//     // Waits until next available screen repaint to run code
+//     window.requestAnimationFrame(function () {});
+//   };
+// });
